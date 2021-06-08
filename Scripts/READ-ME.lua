0b17e3cf26f119c17763f6bf6df191fd53d79f0b
Humza Khan <humza.khan890@gmail.com>
2021-06-08 21:32:45 +0100

    Create READ-ME.lua

---
+++ Scripts/READ-ME.lua
@@ -0,0 +1,34 @@
+--[[
+ __     __        ___   ___   ___     _____ _             _ _       
+ \ \   / /       / _ \ / _ \ / _ \   / ____| |           | (_)      
+  \ \_/ /_ _ _ _| (_) | (_) | | | | | (___ | |_ _   _  __| |_  ___  
+   \   / _` | '__> _ < \__, | | | |  \___ \| __| | | |/ _` | |/ _ \ 
+    | | (_| | | | (_) |  / /| |_| |  ____) | |_| |_| | (_| | | (_) |
+    |_|\__,_|_|  \___/  /_/  \___/  |_____/ \__|\__,_|\__,_|_|\___/ 
+   	
+	This model was made by the Roblox user Yar890.
+	
+
+	========== Version ========== 
+	VERSION: 0.15.0 (Alpha)
+	Release Date: 27/02/2021
+
+	========== Important Info ========== 
+	- For set up instructions, go to: 
+	https://devforum.roblox.com/t/yar890-studio-racing-kit-instructions/741217
+	
+	- For update log, go to: 
+	https://devforum.roblox.com/t/yar890-studio-racing-kit-update-log/741157
+
+	- For latest announcements, visit the Roblox group and scroll down to social links (13+): 
+	https://www.roblox.com/groups/4868458/Yar890-Studio
+	
+	========== Additional links ========== 
+	- Roblox Group (Social links): https://www.roblox.com/groups/4868458/Yar890-Studio
+	- Profile: https://www.roblox.com/users/40912952/profile
+
+	========== Additional Recourses Use ========== 
+	- Car Script: https://developer.roblox.com/en-us/articles/building-carkit-1
+	- Collision groups: https://developer.roblox.com/en-us/articles/Player-Player-Collisions
+	- A-Chassis 6.52S2 kit: https://www.roblox.com/library/426082613/A-Chassis-6-52S2-Kit
+--]]
